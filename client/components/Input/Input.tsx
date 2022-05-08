694b6b91d32b76516f851e673aae92a43c95a41b
Marharyta Leśniak <sirachenko944@gmail.com>
2022-05-08 23:21:56 +0200

    fix onChange

--- client/components/Input/Input.tsx
+++ client/components/Input/Input.tsx
@@ -1,12 +1,13 @@
-import { InputHTMLAttributes, ReactNode, useState } from 'react';
+import { ChangeEventHandler, InputHTMLAttributes, ReactNode, useState } from 'react';
 
 type InputProps = {
   icon?: ReactNode;
   iconPosition?: 'left' | 'right';
   sizeOfInput?: 'md' | 'sm';
+  onChange?: (value: string) => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = ({ placeholder, icon, iconPosition, sizeOfInput, ...props }: InputProps) => {
+export const Input = ({ placeholder, icon, iconPosition, sizeOfInput, onChange, ...props }: InputProps) => {
   const [value, setValue] = useState('');
 
   const size = sizeOfInput === 'sm' ? 'py-1' : 'py-3';
@@ -15,6 +16,11 @@ export const Input = ({ placeholder, icon, iconPosition, sizeOfInput, ...props }
     iconPosition === 'left'
       ? 'absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'
       : 'absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none';
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value);
+    if (onChange) onChange(e.target.value);
+  };
 
   return (
     <div className="mt-1 relative rounded-md shadow-sm w-full">
@@ -25,7 +31,7 @@ export const Input = ({ placeholder, icon, iconPosition, sizeOfInput, ...props }
         className={`bg-background placeholder-border text-white focus:ring-indigo-500 focus:border-indigo-500 block w-full ${size} px-12 sm:text-sm border-border rounded-xl`}
         placeholder={placeholder}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleChange}
         {...props}
       />
     </div>
