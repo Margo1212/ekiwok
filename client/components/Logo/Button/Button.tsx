da79ffd1b0e24e24ddc4fb0faf3a328779cf19af
Ewelina W <ewelina.wystepek@gmail.com>
2022-05-08 16:52:54 +0200

    Button component

---
+++ client/components/Logo/Button/Button.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes, InputHTMLAttributes, ReactNode, useState } from 'react';
+
+
+type ButtonProps = {
+  text: string;
+  transparent?: boolean;
+  buttonSize?: 'sm' | 'md';
+  icon?: ReactNode;
+} & ButtonHTMLAttributes<HTMLInputElement>;
+
+export const Button = ({ text, icon, buttonSize, transparent, onClick }: ButtonProps) => {
+  const size = buttonSize === 'sm' ? 'py-2 text-xl' : 'py-3 text-2xl';
+  const color = transparent ? 'text-primary' : 'text-secondary';
+  const background = transparent ? 'bg-transparent' : 'bg-primary';
+  const hover = 'hover:opacity-75';
+
+  return (
+    <button className={`${background} ${color} ${hover} ${size} px-4 rounded-full border-2 border-primary inline-flex items-center shadow-md`}>
+      {icon}
+      <span>{text}</span>
+    </button>
+  );
+};
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#F2C641',
+        secondary: '#48045C',
+      },
+    },
+    fontFamily: {
+      'Baloo 2': ["Baloo 2", 'cursive'],
+    },
   },
   plugins: [],
 };
