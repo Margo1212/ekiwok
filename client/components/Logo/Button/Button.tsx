efe7d28ca053d25d6533e4e1941e3cdfe4d97422
Ewelina W <ewelina.wystepek@gmail.com>
2022-05-09 15:31:10 +0200

    changes after review

--- client/components/Logo/Button/Button.tsx
+++ client/components/Logo/Button/Button.tsx
@@ -4,18 +4,21 @@ import { ButtonHTMLAttributes, ReactNode } from 'react';
 type ButtonProps = {
   text: string;
   transparent?: boolean;
-  buttonSize?: 'sm' | 'md';
+  size?: 'sm' | 'md';
   icon?: ReactNode;
-} & ButtonHTMLAttributes<HTMLInputElement>;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ text, icon, buttonSize, transparent, onClick }: ButtonProps) => {
+export const Button = ({ text, icon, size: buttonSize, transparent, ...props }: ButtonProps) => {
   const size = buttonSize === 'sm' ? 'py-2 text-xl' : 'py-3 text-2xl';
   const color = transparent ? 'text-primary' : 'text-secondary';
   const background = transparent ? 'bg-transparent' : 'bg-primary';
   const hover = 'hover:opacity-75';
 
   return (
-    <button className={`${background} ${color} ${hover} ${size} px-4 rounded-full border-2 border-primary inline-flex items-center shadow-md`}>
+    <button
+      className={`${background} ${color} ${hover} ${size} px-4 rounded-full border-2 border-primary inline-flex items-center shadow-md`}
+      {...props}
+    >
       {icon}
       <span>{text}</span>
     </button>
