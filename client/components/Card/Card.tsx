8bd8ee286e974f5bf3c29c95b9bf30d5ad2cb758
Marharyta Leśniak <sirachenko944@gmail.com>
2022-05-12 20:54:43 +0200

    default value for padding

--- client/components/Card/Card.tsx
+++ client/components/Card/Card.tsx
@@ -5,7 +5,7 @@ type CardTypeProps = {
   paddingX?: 'sm' | 'md';
 };
 
-const Card = ({ children, paddingX }: CardTypeProps) => {
+const Card = ({ children, paddingX = 'sm' }: CardTypeProps) => {
   const cardPadding = paddingX === 'sm' ? 'px-5' : 'px-14';
 
   return <div className={`${cardPadding} py-6 w-full bg-background-dark opacity-90 rounded-lg shadow-md`}>{children}</div>;
