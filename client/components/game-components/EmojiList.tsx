11b426a8d942d7061cbc245113e65a5872bba862
Marharyta Leśniak <sirachenko944@gmail.com>
2022-05-22 19:53:48 +0200

    add tooltip

--- client/components/game-components/EmojiList.tsx
+++ client/components/game-components/EmojiList.tsx
@@ -1,3 +1,6 @@
+import { listIcons } from '@iconify/react';
+import { Emoji } from 'components/Emoji/emoji';
+
 import { Card } from '../Card/Card';
 import { Text } from '../Text/text';
 
@@ -7,11 +10,17 @@ type EmojiListProps = {
 
 const EmojiList = ({ stretch }: EmojiListProps) => {
   const isStretched = stretch === true ? 'col-span-2' : 'col-span-1';
+  const emojiArr = listIcons('', 'noto');
 
   return (
-    <div className={`${isStretched} mobile:hidden `}>
+    <div className={`${isStretched} mobile:hidden`}>
       <Card paddingX="sm">
         <Text>Kliknij aby wysłać:</Text>
+        <div className="flex flex-wrap">
+          {emojiArr.map((emoji) => (
+            <Emoji key={emoji} size="md" icon={emoji} />
+          ))}
+        </div>
       </Card>
     </div>
   );
