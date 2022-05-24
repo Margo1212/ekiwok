1744a669d91977574830bc3ca3cdefb62996f0b1
Marcin Woś <marcin.wos@coderscrew.pl>
2022-05-24 20:17:40 +0200

    added port variable

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT || 5001;
 
-  await app.listen(5001);
+  await app.listen(port);
 }
 
 bootstrap();
